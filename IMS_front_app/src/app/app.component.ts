59e015bc8863600684561a766158c6e50aa5522f
hatem-marzougui <hatem.adresse@gmail.com>
2024-08-18 18:56:57 +0200

    prepare layout

--- IMS_front_app/src/app/app.component.ts
+++ IMS_front_app/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { LayoutComponent } from './layout/layout.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [LayoutComponent, MatSidenavModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'IMS_front_app';
